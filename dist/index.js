e4d87ac19e79b6c4aa48f82bb0efa00bf877d26e
connect-aima <aimaabbas301@gmail.com>
2025-12-14 14:38:40 +0500

    automated the server restart

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const pets_js_1 = require("./data/pets.js");
+const cors_1 = __importDefault(require("cors"));
 const PORT = 8000;
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     res.json(pets_js_1.pets);
+});
+app.use((req, res) => {
+    res.status(404).json({ message: "No route found" });
 });
 app.listen(PORT, () => {
     console.log("Listening on port: ", PORT);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import type {Express,Request,Response} from 'express'
-import {pets} from './data/pets.js'
+import {pets} from './data/pets'
+import cors from 'cors'
 const PORT = 8000
 const app: Express = express()
-
+app.use(cors())
 app.get('/', (req: Request, res: Response): void => {
   res.json(pets)
+})
+app.use((req:Request, res:Response):void=>{
+  res.status(404).json({message: "No route  found"})
 })
 app.listen(PORT, (): void => {
   console.log("Listening on port: ", PORT)
