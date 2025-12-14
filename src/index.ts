0f62af4ce4107b2f31493dd374ee4739ee6cbfd2
connect-aima <aimaabbas301@gmail.com>
2025-12-14 14:13:37 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import type {Express,Request,Response} from 'express'
+import {pets} from './data/pets.js'
+const PORT = 8000
+const app: Express = express()
+
+app.get('/', (req: Request, res: Response): void => {
+  res.json(pets)
+})
+app.listen(PORT, (): void => {
+  console.log("Listening on port: ", PORT)
+})
