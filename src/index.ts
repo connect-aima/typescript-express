7ea418f99b9dbda3515c8ddc68e589b0bc632cf8
connect-aima <aimaabbas301@gmail.com>
2025-12-15 21:07:36 +0500

    added some typescript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
+console.log('SERVER FILE LOADED')
+
 import express from 'express'
 import type {Express,Request,Response} from 'express'
 import {pets} from './data/pets'
+import type { Pet } from './data/pets'
 import cors from 'cors'
-const PORT = 8000
+const PORT = 8001
 const app: Express = express()
 app.use(cors())
-app.get('/', (req: Request, res: Response): void => {
-  res.json(pets)
+app.get('/', (
+  req:Request<{}, unknown, {}, {species?:string}>, 
+  res:Response<Pet[]>
+):void=> {
+  const {species} = req.query
+
+  let filteredPets:Pet[] = pets
+  
+  if (species){
+    filteredPets = filteredPets.filter((pet:Pet):boolean=>
+      pet.species.toLowerCase() === species.toLowerCase()
+    )
+  }
+  res.json(filteredPets)
 })
-app.use((req:Request, res:Response):void=>{
+app.get('/:id', (req:Request<{id:string}>, res:Response<Pet|{message:string}>):void=>{
+  const {id} = req.params
+  const pet:Pet|undefined = pets.find((pet:Pet):boolean => pet.id.toString() === id) 
+  if (pet){
+    res.json(pet)
+  } else {
+    res.status(404).json({message: "No pet with that ID"})
+  }
+})
+app.use((req:Request, res:Response<{message: string}>):void=>{
   res.status(404).json({message: "No route  found"})
 })
 app.listen(PORT, (): void => {
