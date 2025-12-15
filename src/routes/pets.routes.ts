e3007d51906867d711e5251dfd955c8e9e19a31a
connect-aima <aimaabbas301@gmail.com>
2025-12-15 22:28:30 +0500

    finalized project

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -1,12 +1,11 @@
 import express from 'express'
 import type {Router} from 'express'
-
+import {validateNumericId,pleaseAuth} from '../middleware/pets.middleware'
 import {getPets, getPetById} from '../controllers/pets.controllers'
-
 export const petRouter:Router = express.Router()
 
 
 
 petRouter.get('/', getPets)
 
-petRouter.get('/:id', getPetById)
+petRouter.get('/:id',pleaseAuth, validateNumericId, getPetById)
